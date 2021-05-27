8059c8968e6575d0107b220f82f990706204f94c
Henery Paina <henerypaina@gmail.com>
2021-05-27 10:17:03 +0200

    Adicionando variaveis, criando condicoes para gerar senha, chamando as variaveis nas opecoes de preferencias

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+import 'package:flushbar/flushbar_helper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,6 +9,61 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController _controllerTamanho = TextEditingController(text: "8");
+
+  String senha = "";
+  bool letrasMaiusculas = true;
+  bool letrasMinusculas = true;
+  bool numeros = true;
+  bool simbolos = true;
+
+  senhaGerada(bool letrasMai, bool letrasMin, bool numeros, bool simbolos) {
+    if (letrasMai == false &&
+        letrasMin == false &&
+        numeros == false &&
+        simbolos == false) {
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              content: Text(
+                  "Para gerar senha é necessário que pelo menos uma opção esteja selecionada."),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text("OK"),
+                  onPressed: () => Navigator.pop(context),
+                )
+              ],
+            );
+          });
+    } else {
+      String letrasMaiusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      String letrasMinusculas = "abcdefghijklmnopqrstuvwxyz";
+      String numbers = "0123456789";
+      String symbolos = "!@#\$%&*(){}[]-_=+<>,./";
+
+      int tam = int.parse(_controllerTamanho.text);
+
+      String senhaGer = "";
+      senhaGer += (letrasMai ? letrasMaiusculas : "");
+      senhaGer += (letrasMin ? letrasMinusculas : "");
+      senhaGer += (numeros ? numbers : "");
+      senhaGer += (simbolos ? symbolos : "");
+
+      String pass = "";
+
+      for (int i = 0; i < tam; i++) {
+        int random = Random.secure().nextInt(senhaGer.length);
+        pass += senhaGer[random];
+        setState(() {
+          senha = pass;
+        });
+      }
+
+      return senha;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,7 +93,7 @@ class _HomeState extends State<Home> {
                         borderRadius: BorderRadius.circular(5)),
                     child: Center(
                       child: Text(
-                        "Senha",
+                        "$senha",
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 20,
@@ -79,7 +137,14 @@ class _HomeState extends State<Home> {
                       color: Colors.white,
                     ),
                   ),
-                  onPressed: () {},
+                  onPressed: () {
+                    senhaGerada(
+                      letrasMaiusculas,
+                      letrasMinusculas,
+                      numeros,
+                      simbolos,
+                    );
+                  },
                 ),
               ),
               SizedBox(
@@ -123,11 +188,13 @@ class _HomeState extends State<Home> {
                       children: <Widget>[
                         Text("Letras maiúsculas"),
                         Checkbox(
-                          value: null,
+                          value: letrasMaiusculas,
                           checkColor: Colors.white,
                           activeColor: Colors.blueAccent,
                           onChanged: (bool value) {
-                            setState(() {});
+                            setState(() {
+                              letrasMaiusculas = value;
+                            });
                           },
                         )
                       ],
@@ -137,11 +204,13 @@ class _HomeState extends State<Home> {
                       children: <Widget>[
                         Text("Letras minúsculas"),
                         Checkbox(
-                          value: null,
+                          value: letrasMinusculas,
                           checkColor: Colors.white,
                           activeColor: Colors.blueAccent,
                           onChanged: (bool value) {
-                            setState(() {});
+                            setState(() {
+                              letrasMinusculas = value;
+                            });
                           },
                         )
                       ],
@@ -151,11 +220,13 @@ class _HomeState extends State<Home> {
                       children: <Widget>[
                         Text("Números"),
                         Checkbox(
-                          value: null,
+                          value: numeros,
                           checkColor: Colors.white,
                           activeColor: Colors.blueAccent,
                           onChanged: (bool value) {
-                            setState(() {});
+                            setState(() {
+                              numeros = value;
+                            });
                           },
                         )
                       ],
@@ -165,11 +236,13 @@ class _HomeState extends State<Home> {
                       children: <Widget>[
                         Text("Símbolos"),
                         Checkbox(
-                          value: null,
+                          value: simbolos,
                           checkColor: Colors.white,
                           activeColor: Colors.blueAccent,
                           onChanged: (bool value) {
-                            setState(() {});
+                            setState(() {
+                              simbolos = value;
+                            });
                           },
                         )
                       ],
